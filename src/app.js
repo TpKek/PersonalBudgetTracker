f2db2fbd1aaa21eaca19b11793ac9b35e5addff0
Bertin Dreyer <92753848+TpKek@users.noreply.github.com>
2026-03-21 12:59:20 +0200

    Add logger middleware and update routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
+import 'dotenv/config' // Import dotenv
 import express from 'express';
-import pool from './db/pool.js'
+import pool from './db/pool.js';
 import transactionRoutes from './routes/transactions.js';
+import logger from './middleware/logger.js';
 
 const app = express(); // create express app
-const port = 3000; // specify port
+const port = process.env.PORT; // specify port
 app.use(express.json()); // use json middleware
+app.use(logger);
 app.use('/transactions', transactionRoutes);
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
 
-app.get('/health', (req,res) => {
-    res.json({status: 'ok'});
-})
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
-
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,23 +1,68 @@
 import express from "express"
+import pool from "../db/pool.js"
+
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.json({ success : true, data :[]})
+router.get("/", async (req, res) => {
+    // res.json({ success : true, data :[]})
+ try {
+     const result = await pool.query("SELECT *, amount_cents::int FROM transactions ORDER BY created_at DESC")
+     res.json({ success : true, data : result.rows})
+ } catch (error) {
+    res.status(500).json({ success : false, error : error.message})
+ }
 })
 
-router.get("/:id", (req, res) => {
-    res.json({ success : true, data :{}})
+router.get("/:id", async (req, res) => {
+    // res.json({ success : true, data :{}})
+try {
+    const result = await pool.query("SELECT *, amount_cents::int FROM transactions WHERE id = $1", [req.params.id])
+      if (!result.rows[0]){
+      return res.status(404).json({ success : false, error : "Transaction not found"})
+      }
+
+    res.json({ success : true, data : result.rows[0]})
+
+} catch (error) {
+    res.status(500).json({ success : false, error : error.message})
+}})
+
+router.get('/user/:userId',async (req,res) => {
+try {
+      // res.json({ success : true, data :[]})
+      const result = await pool.query("SELECT *, amount_cents::int FROM transactions WHERE user_id = $1 ORDER BY created_at DESC", [req.params.userId])
+      res.json({ success : true, data : result.rows})
+} catch (error) {
+  res.status(500).json({ success : false, error : error.message})
+}
 })
 
-router.get('/user/:userId',(req,res) => {
-    res.json({ success : true, data :[]})
+router.post('/', async (req, res) => {
+try {
+    // res.json({ success : true, data :{}})
+    const result = await pool.query("INSERT INTO transactions (user_id, amount_cents, description, type, category) VALUES ($1, $2, $3, $4, $5) RETURNING *", [req.body.user_id, req.body.amount_cents, req.body.description, req.body.type, req.body.category])
+    if(!result.rows[0]){
+      return res.status(404).json({ success : false, error : "Transaction not found"})
+      }
+    res.json({ success : true, data : result.rows[0]})
+} catch (error) {
+   res.status(500).json({ success : false, error : error.message})
+}
 })
 
-router.post('/', (req, res) => {
-    res.json({ success : true, data :{}})
-})
+router.patch('/:id/status', async (req,res) => {
+try {
+      // res.json({ success : true, data :{}})
 
-router.patch('/:id/status', (req,res) => {
-    res.json({ success : true, data :{}})
+      const validStatuses = ['pending', 'completed', 'failed']
+      if (!validStatuses.includes(req.body.status)) {
+      return res.status(400).json({ success: false, error: 'Invalid status value' })
+}
+
+      const result = await pool.query("UPDATE transactions SET status = $1 WHERE id = $2 RETURNING *, amount_cents::int", [req.body.status, req.params.id])
+      res.json({ success : true, data : result.rows[0]})
+} catch (error) {
+   res.status(500).json({ success : false, error : error.message})
+}
 })
 export default router
